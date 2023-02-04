83cb458a5497a34d89caa4865e3c38f2ffa75039
Shyllis <91830447+dolpshy@users.noreply.github.com>
2023-02-04 02:14:58 -0700

    bruh

--- source/KadeEngineData.hx
+++ source/KadeEngineData.hx
@@ -6,6 +6,9 @@ class KadeEngineData {
 		if (FlxG.save.data.middleScroll == null)
 			FlxG.save.data.middleScroll = false;
 
+		if (FlxG.save.data.downScroll == null)
+			FlxG.save.data.downScroll = false;
+
 		if (FlxG.save.data.bgNotesAlpha == null)
 			FlxG.save.data.bgNotesAlpha = 0;
 		
